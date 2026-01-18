eb26e67de2bf53982a88c314700a89d25f93a6f6
Fuad2199 <fuad.beybutov099@gmail.com>
2026-01-18 08:11:48 +0400

    first commit

---
+++ src/app/admin/layout.tsx
@@ -0,0 +1,19 @@
+// import Footer from "@/components/Layout/Footer";
+// import Header from "@/components/Layout/Header";
+
+
+export default function AdminLayout({
+  children,
+}: Readonly<{ children: React.ReactNode }>) {
+  return (
+    <div className="flex min-h-screen">
+      <aside className="w-64">Top Sidebar</aside>
+
+      <div className="flex-1 flex flex-col">
+        {/* <Header /> */}
+        <main className="flex-1 p-6">{children}</main>
+        {/* <Footer /> */}
+      </div>
+    </div>
+  );
+}
